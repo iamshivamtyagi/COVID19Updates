81241271617a1527f7a33b56901665557feaf784
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-06-14 22:43:20 +0530

    initial commit

--- app/src/main/java/com/example/covid19update/CountriesActivity.kt
+++ app/src/main/java/com/example/covid19update/CountriesActivity.kt
@@ -1,30 +1,36 @@
 package com.example.covid19update
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_countries.*
-import kotlinx.android.synthetic.main.activity_main.*
-import org.eazegraph.lib.models.PieModel
 import org.json.JSONArray
-import org.json.JSONObject
 
 private const val url = "https://corona.lmao.ninja/v2/countries/"
 
 class CountriesActivity : AppCompatActivity() {
+
+    var list = arrayListOf<Model>()
+
+    var adapter = CountryAdapter(list)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_countries)
 
         setSupportActionBar(toolbar)
+
+        rvList.apply {
+            layoutManager = LinearLayoutManager(this@CountriesActivity)
+            adapter = this@CountriesActivity.adapter
+        }
 
         fetchData()
     }
@@ -62,7 +68,9 @@ class CountriesActivity : AppCompatActivity() {
                             critical
                         )
 
+
                     }
+
                 }
             },
             Response.ErrorListener {
